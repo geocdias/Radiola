08de5aa51255ec1bcbafe6e4be12663d0ec18025
george <geocdias@gmail.com>
2021-01-20 00:20:00 -0300

    Create module AppModule and ServiceModule.kt, implement providers

---
+++ app/src/main/java/com/example/radiola/RadiolaApplication.kt
@@ -0,0 +1,8 @@
+package com.example.radiola
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RadiolaApplication : Application() {
+}
--- app/src/main/java/com/example/radiola/data/remote/MusicDatabase.kt
+++ app/src/main/java/com/example/radiola/data/remote/MusicDatabase.kt
@@ -6,14 +6,14 @@ import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
 
 class MusicDatabase {
-    private val firestore = FirebaseFirestore.getInstance()
-    private val songsCollection = firestore.collection(SONGS_COLLECTION)
+  private val firestore = FirebaseFirestore.getInstance()
+  private val songsCollection = firestore.collection(SONGS_COLLECTION)
 
-    suspend fun getAllSongs(): List<Song> {
-        return try {
-            songsCollection.get().await().toObjects(Song::class.java)
-        }catch (e: Exception) {
-            return emptyList()
-        }
+  suspend fun getAllSongs(): List<Song> {
+    return try {
+      songsCollection.get().await().toObjects(Song::class.java)
+    } catch (e: Exception) {
+      return emptyList()
     }
+  }
 }
