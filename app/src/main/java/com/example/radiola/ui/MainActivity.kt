a28856a613cc818df3179358123ecae5e6d7e214
george <geocdias@gmail.com>
2021-01-30 19:29:21 -0300

    Show Snackbar error, Implement clicklistener play/pause button

--- app/src/main/java/com/example/radiola/ui/MainActivity.kt
+++ app/src/main/java/com/example/radiola/ui/MainActivity.kt
@@ -2,14 +2,18 @@ package com.example.radiola.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.media.session.PlaybackStateCompat
 import androidx.activity.viewModels
+import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.RequestManager
 import com.example.radiola.R
 import com.example.radiola.adapters.SwipeSongAdapter
 import com.example.radiola.data.entities.Song
 import com.example.radiola.player.extentions.toSong
+import com.example.radiola.player.isPlaying
 import com.example.radiola.ui.viewmodels.MainViewModel
 import com.example.radiola.util.Status
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
@@ -27,11 +31,30 @@ class MainActivity : AppCompatActivity() {
 
   private var currentPlayingSong: Song? = null
 
+  private var playbackState: PlaybackStateCompat? = null
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
     vpSong.adapter = swipeSongAdapter
+
+    vpSong.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+      override fun onPageSelected(position: Int) {
+        super.onPageSelected(position)
+        if(playbackState?.isPlaying == true) {
+          viewModel.playOrToggleSong(swipeSongAdapter.songs[position])
+        } else {
+          currentPlayingSong = swipeSongAdapter.songs[position]
+        }
+      }
+    })
+
+    ivPlayPause.setOnClickListener {
+      currentPlayingSong?.let {
+        viewModel.playOrToggleSong(it, true)
+      }
+    }
 
     subcribeToObservers()
   }
@@ -60,7 +83,6 @@ class MainActivity : AppCompatActivity() {
           Status.LOADING -> Unit
           Status.ERROR -> Unit
         }
-
       }
     }
 
@@ -71,6 +93,38 @@ class MainActivity : AppCompatActivity() {
       glide.load(currentPlayingSong?.imageUrl).into(ivCurSongImage)
 
       switchViewPagerToCurrentSong(currentPlayingSong ?: return@observe)
+    }
+
+    viewModel.playbackState.observe(this) {
+      playbackState = it
+      ivPlayPause.setImageResource(
+        if(playbackState?.isPlaying == true) R.drawable.ic_pause else R.drawable.ic_play
+      )
+      glide.load(currentPlayingSong?.imageUrl).into(ivCurSongImage)
+    }
+
+    viewModel.isConnected.observe(this) {
+      it?.getContentIfNotHandled()?.let { result ->
+        when(result.status) {
+          Status.ERROR -> Snackbar.make(
+            rootLayout,
+            result.message ?: "Ops! Aconteceu algo errado.",
+            Snackbar.LENGTH_SHORT
+          ).show()
+        }
+      }
+    }
+
+    viewModel.networkError.observe(this) {
+      it?.getContentIfNotHandled()?.let { result ->
+        when(result.status) {
+          Status.ERROR -> Snackbar.make(
+            rootLayout,
+            result.message ?: "Ops! Aconteceu um erro de conexão",
+            Snackbar.LENGTH_SHORT
+          ).show()
+        }
+      }
     }
   }
 }
